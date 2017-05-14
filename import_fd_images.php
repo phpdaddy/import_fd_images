9633876058a0495911341caaa30c0f2b31f506a3
Maksym Prysiazhnyi <maksym.prysiazhnyi@edgedesign.cz>
2017-05-14 17:26:27 +0200

    Upload images based on product type

--- import_fd_images.php
+++ import_fd_images.php
@@ -12,6 +12,7 @@ add_action('admin_menu', 'plugin_setup_menu');
 function plugin_setup_menu()
 {
     add_menu_page('Import FD images Page', 'Import FD images', 'manage_options', 'import-fd-images', 'submit_button_admin_page');
+
 }
 
 function submit_button_admin_page()
@@ -20,18 +21,31 @@ function submit_button_admin_page()
         wp_die(__('You do not have sufficient pilchards to access this page.'));
     }
 
+    $url = 'http://fashiondropshippers.com/media/feed/fashiondropshippers-feeds.csv';
+    $data = @file_get_contents($url);
+
+    $csv = new parseCSV($data);
+    $csv->auto();
+
+    if (isset($_POST['submit_button']) && check_admin_referer('submit_button_clicked')) {
+        submit_button_action($csv);
+        return;
+    }
 
     echo '<div class="wrap">';
 
     echo '<h2>Import FD iamges</h2>';
 
-    if (isset($_POST['submit_button']) && check_admin_referer('submit_button_clicked')) {
-        submit_button_action();
-    }
+    $list = get_product_types_list($csv);
 
     echo '<form action="admin.php?page=import-fd-images" method="post">';
     echo 'CSV url: ';
-    echo '<input type="text" value="' . $_POST['csv_url'] . '" name="csv_url" />';
+    echo '<select value="' . $_POST['product_type'] . '" name="product_type" >';
+    echo '<option>All</option>';
+    foreach ($list as $item) {
+        echo '<option>' . $item . '</option>';
+    }
+    echo '</select>';
     wp_nonce_field('submit_button_clicked');
     echo '<input type="hidden" value="true" name="submit_button" />';
     submit_button('Import images');
@@ -41,39 +55,51 @@ function submit_button_admin_page()
 
 }
 
-function submit_button_action()
+function get_product_types_list($csv)
+{
+    $product_types = [];
+    foreach ($csv->data as $row) {
+        $product_types[] = $row['product_type'];
+    }
+    $product_types = array_unique($product_types);
+    sort($product_types);
+    return $product_types;
+}
+
+function submit_button_action($csv)
 {
     echo '<div id="message" class="updated fade"><p>'
         . 'Images were imported.' . '</p></div>';
 
-    $data = @file_get_contents($_POST['csv_url']);
+    $product_type = $_POST['product_type'];
 
-    $csv = new parseCSV($data);
-    $csv->auto();
-    $i = 0;
     $ids = [];
     foreach ($csv->data as $row) {
-        $ids[] = $row['id'];
+        if ($row['product_type'] === $product_type || $product_type === 'All') {
+            $ids[] = $row['id'];
+        }
     }
     $ids = array_unique($ids);
 
     $imagesFolder = WP_CONTENT_DIR . '/images/';
-    rmdir($imagesFolder);
+
     foreach ($ids as $id) {
         try {
             $zipLocalFile = download_zip($id, $imagesFolder);
 
             $path = pathinfo(realpath($zipLocalFile), PATHINFO_DIRNAME);
+
+            $productImagesFolder = $imagesFolder . '/' . basename($zipLocalFile, '.zip');
+            if (file_exists($productImagesFolder)) {
+                rrmdir($productImagesFolder);
+            }
+
             $productImagesFolder = extract_zip($zipLocalFile, $path);
 
             unlink($zipLocalFile);
             rename_images($productImagesFolder);
 
             echo '<div class="notice notice-success">Successfully imported images for ' . $id . '</div>';
-            $i++;
-            if ($i == 6) {
-                break;
-            }
         } catch (RuntimeException $ex) {
             echo '<div class="notice notice-error">' . $ex->getMessage() . '</div>';
         }
@@ -123,6 +149,25 @@ function rename_images($imagesFolder)
         }
         closedir($handle);
     }
+}
+
+/**
+ * Recursively removes a folder along with all its files and directories
+ *
+ * @param String $path
+ */
+function rrmdir($path)
+{
+    // Open the source directory to read in files
+    $i = new DirectoryIterator($path);
+    foreach ($i as $f) {
+        if ($f->isFile()) {
+            unlink($f->getRealPath());
+        } else if (!$f->isDot() && $f->isDir()) {
+            rrmdir($f->getRealPath());
+        }
+    }
+    rmdir($path);
 }
 
 ?>
